5111c92e089737ee89bfb90f01462e94087d8e91
Piotr Batko <batkopiotr.mail@gmail.com>
2020-05-17 16:42:42 +0200

    Change tabs to spaces in Board.cpp

--- src/Board.cpp
+++ src/Board.cpp
@@ -33,36 +33,45 @@ std::size_t Board::getWidth() const
     return m_Width;
 }
 
-bool Board::IsFieldOnBoard(const std::size_t x, const std::size_t y) const {
-	if (   x >= 0u && x < m_Width
-	    && y >= 0u && y < m_Height) {
-		return true;
-	}
-	return false;
+bool Board::IsFieldOnBoard(const std::size_t x, const std::size_t y) const
+{
+    if (   0U <= x && x < m_Width
+        && 0U <= y && y < m_Height)
+    {
+        return true;
+    }
+
+    return false;
 }
 
-bool Board::IsFieldEmpty(const std::size_t x, const std::size_t y, bool& executionStatus, bool logErrorWhenOccured) const {
-	if (!IsFieldOnBoard(x, y)) {
-		executionStatus = false;
-		if (logErrorWhenOccured) {
-			LOG_ERROR("Field ", x, ", ", y, ", is not on board.");
-		}
-		return false;
-	}
-	executionStatus = true;
+bool Board::IsFieldEmpty(const std::size_t x, const std::size_t y, bool & executionStatus, bool logErrorWhenOccured) const
+{
+    if (!IsFieldOnBoard(x, y))
+    {
+        executionStatus = false;
+        if (logErrorWhenOccured)
+        {
+            LOG_ERROR("Field ", x, ", ", y, ", is not on board.");
+        }
+        return false;
+    }
+    executionStatus = true;
 
-	Field fieldValue = at(x, y);
-	if (fieldValue == Field::Empty) {
-		return true;
-	}
-	return false;
+    Field fieldValue = at(x, y);
+    if (fieldValue == Field::Empty)
+    {
+        return true;
+    }
+    return false;
 }
 
-bool Board::SetField(const std::size_t x, const std::size_t y, const Field fieldValueToSet) {
-	if (x > m_Width || y > m_Height) {
-		LOG_ERROR("x > m_Width || y > m_Height");
-		return false;
-	}
-	m_Fields[y * m_Height + x] = fieldValueToSet;
-	return true;
+bool Board::SetField(const std::size_t x, const std::size_t y, const Field fieldValueToSet)
+{
+    if (x > m_Width || y > m_Height)
+    {
+        LOG_ERROR("x > m_Width || y > m_Height");
+        return false;
+    }
+    m_Fields[y * m_Height + x] = fieldValueToSet;
+    return true;
 }
