75b1814441289caf2f998f6782c32c9d67ec7f9c
Piotr Batko <batkopiotr.mail@gmail.com>
2020-07-11 20:41:57 +0200

    Use FindAdjacent during insertion

--- src/BatoBot/Engine/AdjacentEmptyFieldsTracker.cpp
+++ src/BatoBot/Engine/AdjacentEmptyFieldsTracker.cpp
@@ -57,29 +57,7 @@ std::vector<Coordinates>::iterator AdjacentEmptyFieldsTracker::FindAdjacent(Coor
 
 void AdjacentEmptyFieldsTracker::InsertAdjacentIfNotPresent(Coordinates move)
 {
-    const auto MoveIter = std::lower_bound(
-        std::begin(m_AdjacentFields),
-        std::end(m_AdjacentFields),
-        move,
-        [](const Coordinates& lhs, const Coordinates& rhs)
-        {
-            if (lhs.y < rhs.y)
-            {
-                return true;
-            }
-            else if (lhs.y > rhs.y)
-            {
-                return false;
-            }
-
-            if (lhs.x < rhs.x)
-            {
-                return true;
-            }
-
-            return false;
-        }
-    );
+    const auto MoveIter = FindAdjacent(move);
 
     if (MoveIter == std::end(m_AdjacentFields))
     {
