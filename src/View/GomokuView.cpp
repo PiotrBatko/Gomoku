e693f346b254276e65c97e2ec08235760c6cd412
Piotr Batko <batkopiotr.mail@gmail.com>
2020-07-14 19:51:20 +0200

    Add window creation

--- src/View/GomokuView.cpp
+++ src/View/GomokuView.cpp
@@ -1,5 +1,19 @@
 #include "GomokuView.hpp"
 
+#include "AppConfig/FileAppConfigContainer.hpp"
+
 void GomokuView::Run()
 {
+    CreateWindow();
+}
+
+void GomokuView::CreateWindow()
+{
+    m_Window.create(
+        sf::VideoMode(
+            FieldWidthInPixels * fileAppConfigContainer.BoardSize,
+            FieldHeightInPixels * fileAppConfigContainer.BoardSize
+        ),
+        "Gomoku Bot Battle"
+    );
 }
--- src/View/GomokuView.hpp
+++ src/View/GomokuView.hpp
@@ -1,11 +1,22 @@
 #ifndef GOMOKUVIEW_HPP
 #define GOMOKUVIEW_HPP
 
+#include "SFML/Graphics/RenderWindow.hpp"
+
 class GomokuView
 {
 public:
 
     void Run();
+
+private:
+
+    void CreateWindow();
+
+    static constexpr std::size_t FieldWidthInPixels = 25u;
+    static constexpr std::size_t FieldHeightInPixels = FieldWidthInPixels;
+
+    sf::RenderWindow m_Window;
 };
 
 #endif
