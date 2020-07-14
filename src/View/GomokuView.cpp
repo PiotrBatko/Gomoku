3f53f29b6049906d3737669b55b3f02f34d2f88b
Piotr Batko <batkopiotr.mail@gmail.com>
2020-07-14 20:21:13 +0200

    Add fields drawing

--- src/View/GomokuView.cpp
+++ src/View/GomokuView.cpp
@@ -4,6 +4,16 @@
 
 #include "AppConfig/FileAppConfigContainer.hpp"
 
+GomokuView::GomokuView() :
+    m_FieldView(
+        sf::Vector2f(
+            FieldWidthInPixels,
+            FieldHeightInPixels
+        )
+    )
+{
+}
+
 void GomokuView::Run()
 {
     CreateWindow();
@@ -37,5 +47,28 @@ void GomokuView::MainLoop()
                 break;
             }
         }
+
+        Draw();
     }
+}
+
+void GomokuView::Draw()
+{
+    m_Window.clear(sf::Color::Black);
+
+    for (int y = 0; y < fileAppConfigContainer.BoardSize; ++y)
+    {
+        for (int x = 0; x < fileAppConfigContainer.BoardSize; ++x)
+        {
+            sf::Vector2f fieldCenter(
+                FieldWidthInPixels / 2 + x * FieldWidthInPixels,
+                FieldHeightInPixels / 2 + y * FieldHeightInPixels
+            );
+
+            m_FieldView.SetPosition(fieldCenter);
+            m_Window.draw(m_FieldView);
+        }
+    }
+
+    m_Window.display();
 }
--- src/View/GomokuView.hpp
+++ src/View/GomokuView.hpp
@@ -3,21 +3,26 @@
 
 #include "SFML/Graphics/RenderWindow.hpp"
 
+#include "FieldView.hpp"
+
 class GomokuView
 {
 public:
 
+    GomokuView();
     void Run();
 
 private:
 
     void CreateWindow();
     void MainLoop();
+    void Draw();
 
     static constexpr std::size_t FieldWidthInPixels = 25u;
     static constexpr std::size_t FieldHeightInPixels = FieldWidthInPixels;
 
     sf::RenderWindow m_Window;
+    FieldView m_FieldView;
 };
 
 #endif
