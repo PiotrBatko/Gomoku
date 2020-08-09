89c191b9e0f86a5b9b880627245932cb69b897a0
Piotr Batko <batkopiotr.mail@gmail.com>
2020-08-09 14:46:38 +0200

    Add GameFinished and Terminate to GameView interface

--- src/GameView.hpp
+++ src/GameView.hpp
@@ -14,6 +14,14 @@ public:
     /// board dimensions.
     virtual void GameStarted(std::size_t numberOfColumns, std::size_t numberOfRows) = 0;
 
+    /// Invoked at the end of each game. Game may be finished because one player
+    /// set 5 pawns in a row, time runs out, players accept draw or maybe even
+    /// something else. Please not confuse this event with Terminate.
+    virtual void GameFinished() = 0;
+
+    /// Invoked when view should be terminated.
+    virtual void Terminate() = 0;
+
     virtual ~GameView() = default;
 
     void SetGameModel(GameModel& model);
