be93a9bcb17195aa66fa9d0f13cba71ceb71a49e
Piotr Batko <batkopiotr.mail@gmail.com>
2020-08-09 19:42:03 +0200

    Make it easier to use PawnColor and Field together

--- src/Field.hpp
+++ src/Field.hpp
@@ -1,11 +1,43 @@
-#ifndef FIELD_HPP_
-#define FIELD_HPP_
+#ifndef FIELD_HPP
+#define FIELD_HPP
 
-enum class Field
+#include "PawnColor.hpp"
+
+class Field
 {
-    Empty,
-    White,
-    Black,
+public:
+
+    enum Value
+    {
+        White = static_cast<int>(PawnColor::White),
+        Black = static_cast<int>(PawnColor::Black),
+        Empty,
+    };
+
+    constexpr Field() = default;
+    constexpr Field(Value value) :
+        m_Value(value)
+    {
+    }
+
+    constexpr Field(PawnColor color) :
+        m_Value(color == PawnColor::White ? White : Black)
+    {
+    }
+
+    constexpr bool operator==(const Field rhs) const
+    {
+        return m_Value == rhs.m_Value;
+    }
+
+    constexpr bool operator!=(const Field rhs) const
+    {
+        return not (*this == rhs);
+    }
+
+private:
+
+    Value m_Value = Empty;
 };
 
 #endif
