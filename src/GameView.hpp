bf4b7efcb7007d63ccae19e15118563c05cfac36
Piotr Batko <batkopiotr.mail@gmail.com>
2020-08-09 15:43:33 +0200

    Add PawnAdded to GameView interface

--- src/GameView.hpp
+++ src/GameView.hpp
@@ -3,7 +3,9 @@
 
 #include <cstddef>
 
+#include "Coordinates.hpp"
 #include "GameModel.hpp"
+#include "PawnColor.hpp"
 
 /// Game model interface for views
 class GameView
@@ -13,6 +15,9 @@ public:
     /// Invoked at the beginning of each game. Notifies view about
     /// board dimensions.
     virtual void GameStarted(std::size_t numberOfColumns, std::size_t numberOfRows) = 0;
+
+    /// Invoked after each pawn addition.
+    virtual void PawnAdded(PawnColor pawnColor, Coordinates coordinates) = 0;
 
     /// Invoked at the end of each game. Game may be finished because one player
     /// set 5 pawns in a row, time runs out, players accept draw or maybe even
